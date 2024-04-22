c463146a3eba56fb9b98b1cd075e9153ca268c4f
R4inssss <145927774+R4inssss@users.noreply.github.com>
2024-04-22 16:10:11 -0700

    Add files via upload

---
+++ Week 4/percentiles.py
@@ -0,0 +1,13 @@
+# Given dataset
+data = [1182, 1198, 1215, 1388, 1536, 1612, 1650, 1841, 1904, 2000, 2123, 2151, 2307, 2425, 2425, 2499, 2540, 2625, 2800, 3212]
+
+# Percentile to calculate
+percentile = 80
+
+# Calculate the position i for the 80th percentile
+n = len(data)
+i = (percentile / 100) * (n + 1)
+
+# Show calculated index for the 80th percentile
+
+print(i)
