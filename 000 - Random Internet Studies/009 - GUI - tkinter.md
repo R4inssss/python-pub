80f97d18e2e92c62454088280ba36b238a28337e
R4inssss <145927774+R4inssss@users.noreply.github.com>
2024-06-20 09:14:51 -0700

    tkinter research

---
+++ 000 - Random Internet Studies/009 - GUI - tkinter.md
@@ -0,0 +1,23 @@
+# tkinter
+
+```python
+# installation
+pip install tk
+```
+RIGHT, i thought it was tkinter but it's tk
+
+First : 
+https://docs.python.org/3/library/tkinter.html#important-tk-concepts
+
+Using [this video](https://www.youtube.com/watch?v=kvd6GQZASno) as the primary source and the documentations as subsequent references.
+First thing I found out
+you can't do 900 x 900, spaces are not allowed. 
+
+The labels need a parent object, hence they referenced it to the "window".
+![[Pasted image 20240619205459.png]]
+The area is mapped into columns and rows.
+
+get = a method to get a value.
+
+showinfo = messagebox
+you have to import it from tkinter
---
+++ Week 12/Project 1/calculator_appv1.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import messagebox
+import calculator_appv2
+
+
+class CalculatorApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Statistics Calculator')
+        self.geometry('400x600')
+
+        self.expression = ""
+        self.entry = tk.Entry(self, font=("Arial", 24), bd=10, insertwidth=2, width=14, borderwidth=4)
+        self.entry.grid(row=0, column=0, columnspan=5)
+
+        self.calc_btn()
+
+    def calc_btn(self):
+        button_texts = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', '.', '=', '+'
+        ]
+
+        row = 1
+        col = 0
+        for text in button_texts:
+            button = tk.Button(self, text=text, padx=20, pady=20, font=('Arial', 18),
+                               command=lambda t=text: self.on_button_click(t))
+            # TODO: Create on button clicked function
+            # button orientation to cardinal directions
+            button.grid(row=row, column=col, sticky="nsew")
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
+
+#            row += 1 # future planning for my functions
+
+
+if __name__ == "__main__":
+    app = CalculatorApp()
+    app.mainloop()
