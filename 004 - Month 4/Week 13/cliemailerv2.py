4bc7223920c6f3ddfaf264208953d71f86792c7a
R4inssss <145927774+R4inssss@users.noreply.github.com>
2024-06-29 08:55:21 -0700

    Ever wanted to really spam someone?? Well now you can! Change i to be any number, or time.sleep to be a lower int if you want.

---
+++ 004 - Month 4/Week 13/cliemailerv2.py
@@ -0,0 +1,88 @@
+#! python3
+#       cliemailerv2.py - takes an email and string of text from cli
+#       uses selenium to log into email and send an email given the email and string from cli
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import getpass
+import time
+
+
+def cliemail():
+    email = input('Enter your email: ')
+    password = getpass.getpass('Enter your password: ')
+    recipient = input('Enter recipient email: ')
+    subject = input('Enter your subject: ')
+    body = input('Enter your message: ')
+    browser = webdriver.Firefox()
+    browser.get('https://mail.google.com')
+    i = 0
+
+    # Email
+    emailElem = browser.find_element(By.ID, 'identifierId')
+    emailElem.send_keys(email)
+    emailElem.send_keys(Keys.RETURN)
+
+    # Password
+    passwordElem = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.NAME, 'Passwd'))
+    )
+    passwordElem.send_keys(password)
+    passwordElem.send_keys(Keys.RETURN)
+
+    while True:
+        # Find Compose Button
+        composeElem = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '.T-I-KE'))
+        )
+
+        composeElem.send_keys(Keys.RETURN)
+
+        # Find email field
+        toElem = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//input[@aria-label="To recipients"]'))
+        )
+        toElem.send_keys(recipient)
+
+        # Find subject field
+        toSub = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'subjectbox'))
+        )
+        toSub.send_keys(subject + str(i))
+        i += 1
+
+        # Find Message body
+        toBody = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@aria-label="Message Body"]'))
+        )
+        toBody.send_keys(body)
+
+        # Find send button
+
+        toSend = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@role="button" and text()="Send"]'))
+        )
+        toSend.send_keys(Keys.RETURN)
+
+        time.sleep(5)
+
+        if i >= 10:
+            break
+
+
+cliemail()
+
+# Debug Code
+# def test_pass():
+#     password = getpass.getpass('Enter your password: ')
+#     print(password)
+#     print(type(password))
+#
+# test_pass()
+
+# Userid variable
+# Password variable
