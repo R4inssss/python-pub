eacd979e53225773be767d2d1b455ce9dfecfbde
R4inssss <145927774+R4inssss@users.noreply.github.com>
2024-07-02 08:58:53 -0700

    More API things

--- 004 - Month 4/Week 14/API/main.py
+++ 004 - Month 4/Week 14/API/main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
 def hello():
     return {"message": "welcome to API"}
+
+
+@app.get("/home")
+def get_posts():
+    return {"message": "Home"}
+
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())  # Converts to dictionary
+    return {"data": post}
+
