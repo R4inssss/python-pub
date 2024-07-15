23d65fe4729dc719be098f2b0c83a0036a5c7ce4
R4inssss <145927774+R4inssss@users.noreply.github.com>
2024-07-15 08:35:47 -0700

    Set up some hashing + Oauth

--- 004 - Month 4/Week 14/API/app/routers/auth.py
+++ 004 - Month 4/Week 14/API/app/routers/auth.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
-
-
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(
     prefix="/login",
@@ -11,14 +10,16 @@ router = APIRouter(
 
 
 @router.post('/')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Credentials invalid')
 
     if not utils.verify_password(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Credentials invalid')
 
-    # create token
-    # return token
-    return{"token": "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
