d39537e14ebdf45e7233b88c74ca165a79a16f49
R4inssss <145927774+R4inssss@users.noreply.github.com>
2024-07-17 05:34:24 -0700

    Some dependency using oauth | set up dev environment

--- 004 - Month 4/Week 14/API/app/oauth2.py
+++ 004 - Month 4/Week 14/API/app/oauth2.py
@@ -3,15 +3,16 @@
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')  # login endpoint
 
 SECRET_KEY = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7'
 ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
 # def create_access_token(data: dict):  # here, data is our payload
@@ -39,10 +40,10 @@ def verify_access_token(token: str, credentials_exception):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: str = payload.get("user_id")
         print(f"Payload: {payload}")
-        print(f"ID extracted from token: {id}, Type: {type(id)}")
+        print(f"ID extracted from token: {id}, Type: {type(id)}")  # Debug code
         if id is None:
             raise credentials_exception
-        print(f"ID extracted from token: {id}, Type: {type(id)}")
+        print(f"ID extracted from token: {id}, Type: {type(id)}")  # Debug code
         token_data = schemas.TokenData(id=id)
     except JWTError as e:
         print(f"JWTError: {e}")  # Print the JWTError if it occurs
@@ -51,8 +52,10 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail=f"Could not validate credentials.",
                                           headers={"WWW-Authenticate": "Bearer"})
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
--- 004 - Month 4/Week 14/API/app/routers/post.py
+++ 004 - Month 4/Week 14/API/app/routers/post.py
@@ -12,7 +12,7 @@ router = APIRouter(
 
 # Find all posts
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
@@ -22,12 +22,12 @@ def get_posts(db: Session = Depends(get_db)):
 # Create posts
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
-                 user_id: int = Depends(oauth2.get_current_user)):
+                 current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    print(user_id)
+    print(current_user.email)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -38,7 +38,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
 
 # Get one post
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()  # equivalent of doing a where
 
     if not post:
@@ -49,7 +49,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 # Delete posts
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
@@ -61,7 +61,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 # Update posts
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     post_query.update(updated_post.dict(), synchronize_session=False)
