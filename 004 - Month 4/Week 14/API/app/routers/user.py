30fb57986b45a14bcc9b75eb69313e305b764a03
R4inssss <peterbuinam@gmail.com>
2024-07-27 21:29:19 -0700

    starting over because errors

--- 004 - Month 4/Week 14/API/app/routers/user.py
+++ 004 - Month 4/Week 14/API/app/routers/user.py
@@ -15,6 +15,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # Hashing our password - user.password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
+
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
@@ -24,10 +25,9 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get('/{id}', response_model=schemas.UserOut)
-def get_user(id: int, db: Session = Depends(get_db)):
+def get_user(id: int, db: Session = Depends(get_db), ):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"user with id {id} not found")
     return user
-
--- 004 - Month 4/Week 14/API/tests/test_users.py
+++ 004 - Month 4/Week 14/API/tests/test_users.py
@@ -1,48 +1,41 @@
-import pytest
-from jose import jwt
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.main import app
 from app import schemas
 from app.config import settings
 
 
-# def test_root(client):
-
-#     res = client.get("/")
-#     print(res.json().get('message'))
-#     assert res.json().get('message') == 'Hello World'
-#     assert res.status_code == 200
-
-
-def test_create_user(client):
-    res = client.post(
-        "/users/", json={"email": "test1111@gmail.com", "password": "123456"})
-
-    new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "test1111@gmail.com"
-    assert res.status_code == 201
+SQLALCHEMY_DATABASE_URL = (f'postgresql://{settings.database_username}:{settings.database_password}'
+                           f'@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 
-def test_login_user(test_user, client):
-    res = client.post(
-        "/login", data={"username": test_user['email'], "password": test_user['password']})
-    login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token,
-                         settings.secret_key, algorithms=[settings.algorithm])
-    id = payload.get("user_id")
-    assert id == test_user['id']
-    assert login_res.token_type == "bearer"
+# Gets session from database
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+client = TestClient(app)
+
+def test_root():
+    res = client.get("/")
+    print(res.status_code)
+    print(res.json())
     assert res.status_code == 200
+    assert res.json().get('message') == 'Volume Bind with tags'
 
-
-@pytest.mark.parametrize("email, password, status_code", [
-    ('wrongemail@gmail.com', 'password123', 403),
-    ('test1111@gmail.com', 'wrongpassword', 403),
-    ('wrongemail@gmail.com', 'wrongpassword', 403),
-    (None, '123456', 422),
-    ('test1111@gmail.com', None, 422)
-])
-def test_incorrect_login(test_user, client, email, password, status_code):
-    res = client.post(
-        "/login", data={"username": email, "password": password})
-
-    assert res.status_code == status_code
-    # assert res.json().get('detail') == 'Invalid Credentials'
+def test_create_user():
+    res = client.post("/users/create", json=
+                      {"email": "test123@example.com", "password": "password123", "phone_number": "12345"})
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "test123@example.com"
+    assert res.status_code == 201
