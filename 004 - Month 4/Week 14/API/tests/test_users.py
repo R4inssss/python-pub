d65c2389dcf1c0dbe528551b13780f6318b7ea61
R4inssss <peterbuinam@gmail.com>
2024-07-27 22:12:22 -0700

    Fixed testdb thanks to some help

--- 004 - Month 4/Week 14/API/tests/test_users.py
+++ 004 - Month 4/Week 14/API/tests/test_users.py
@@ -1,39 +1,15 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.main import app
 from app import schemas
-from app.config import settings
+from .database import client, session
 
 
-SQLALCHEMY_DATABASE_URL = (f'postgresql://{settings.database_username}:{settings.database_password}'
-                           f'@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
-# Gets session from database
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.status_code)
     print(res.json())
     assert res.status_code == 200
     assert res.json().get('message') == 'Volume Bind with tags'
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/create", json=
                       {"email": "test123@example.com", "password": "password123", "phone_number": "12345"})
     new_user = schemas.UserOut(**res.json())
